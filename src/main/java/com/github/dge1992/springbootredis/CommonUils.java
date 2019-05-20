f48c1fdd0c062861ffe781909e8d7e9fa6a012fc
dge1992 <dge_1992@163.com>
2019-05-20 15:55:48 +0800

    加一个随机数防止缓存雪崩

--- src/main/java/com/github/dge1992/springbootredis/CommonUils.java
+++ src/main/java/com/github/dge1992/springbootredis/CommonUils.java
@@ -1,5 +1,6 @@
 package com.github.dge1992.springbootredis;
 
+import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -16,6 +17,15 @@ public class CommonUils {
      */
     public static String getUUID(){
         return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /**
+     * @author dongganen
+     * @date 2019/5/20
+     * @desc: 获取一个随机数
+     */
+    public static int getRandomInt(Integer i){
+        return new Random().nextInt(i) + 1;
     }
 
 }
